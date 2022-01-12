92c7360b20ee2cc83bc8e6695720bffd7f54c526
Caio César Oliveira <54439337+oCaioOliveira@users.noreply.github.com>
2022-01-12 01:28:42 -0300

    Versão 1.0
    Versão inicial de um teste automatizado para WEB.

---
+++ TestesAutomatizados/src/test/java/stepsDefinitions/Hooks.java
@@ -0,0 +1,40 @@
+package stepsDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+import static utils.Utils.*;
+
+public class Hooks {
+
+	@Before()
+	public void setUp() {
+		acessarSistema();
+	}
+	
+//	@Before(order = 2, value = "@login")
+//	public void setUp2() {
+//		acessarSistema();
+//	}
+//	
+//	@Before(order = 3, value = "@login")
+//	public void setUp3() {
+//		acessarSistema();
+//	}
+	
+	@After()
+	public void tearDown() {
+//		driver.quit();
+	}
+	
+//	@After(order = 1, value = "@login")
+//	public void tearDown2() {
+//		
+//	}
+//	
+//	@After(order = 0, value = "@login")
+//	public void tearDown3() {
+//		
+//	}
+	
+}
